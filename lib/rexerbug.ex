baf644bfab54459feb76191ce8e3800a53ef7397
Johnson Denen <johnson.denen@gmail.com>
2022-08-13 16:20:11 -0400

    Add Rexerbug.trace/2

--- lib/rexerbug.ex
+++ lib/rexerbug.ex
@@ -1,4 +1,8 @@
 defmodule Rexerbug do
+  def trace(pattern, opts \\ []) do
+    rexbug().start(pattern, opts)
+  end
+
   @doc false
   def rexbug do
     Application.get_env(:rexerbug, :rexbug, Rexbug)
--- test/rexerbug_test.exs
+++ test/rexerbug_test.exs
@@ -1,6 +1,25 @@
 defmodule RexerbugTest do
   use ExUnit.Case
 
+  import Mox
+
+  setup :verify_on_exit!
+
+  describe "start/2" do
+    test "passes trace pattern to Rexbug" do
+      pattern = "Map.new/0 :: stack"
+      expect(RexbugMock, :start, fn ^pattern, _ -> {100, 42} end)
+
+      assert {_, _} = Rexerbug.trace(pattern)
+    end
+
+    test "passes options to Rexbug" do
+      opts = [time: 1_000]
+      expect(RexbugMock, :start, fn _, ^opts -> {100, 42} end)
+      assert {_, _} = Rexerbug.trace("Map.new/0 :: stack", opts)
+    end
+  end
+
   describe "rexbug/0" do
     test "returns configured rexbug client" do
       assert Rexerbug.rexbug() == RexbugMock
